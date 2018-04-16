f5341b84c9a4fa476d21f515aefb0a462cb55971
Manikanta Kondeti <manikanta.kondeti@sprinklr.com>
2018-04-16 23:38:20 +0530

    Boilerplate code

---
+++ frontend/src/components/Sample.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Sample  = () => {
+    return (
+        <div>
+            <h2>Hello World! React </h2>
+            <p>The page you're looking for exists.</p>
+        </div>
+    )
+}
+
+export default Sample
